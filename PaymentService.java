ec8def70106db3e37c7673d376075cb5fda4b728
sourabh-kelani <kelanisourabh@gmail.com>
2026-08-27 17:57:23 +0530

    Added functions

--- PaymentService.java
+++ PaymentService.java
@@ -2,6 +2,16 @@ public class PaymentService {
 
     public void processPayment() {
         System.out.println("Payment processed");
-		System.out.println("Payment processed");
     }
+	
+	public void refundPayment() {
+        System.out.println("Payment refunded");
+    }
+	public void cancelPayment() {
+    System.out.println("Payment cancelled");
+}
+
+public void retryPayment() {
+    System.out.println("Retrying payment");
+}
 }
